e49c6fa8505ed071ae6c0bf004a975c744efda41
sabinonweb <sabinonweb7@gmail.com>
2025-12-26 06:11:24 +0545

    [Update: Server]

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,16 @@ use no_cap::{
     types::{args::Args, blockchain::TransactionMessage},
     utils::reqwest::get_external_ip,
 };
-use std::sync::Arc;
+use std::{net::SocketAddr, sync::Arc};
 use tokio::{
     io::AsyncWriteExt,
     net::TcpListener,
     sync::{mpsc, Mutex},
 };
+use tower_http::cors::{Any, CorsLayer};
 
 #[derive(Clone)]
 struct AppState {
-    blockchain: Arc<Mutex<Blockchain>>,
     pool: Arc<Mutex<ConnectionPool>>,
     p2p: Arc<Mutex<P2PProtocol>>,
     ws_peers: Arc<Mutex<Vec<mpsc::UnboundedSender<Message>>>>,
@@ -36,19 +36,17 @@ struct AppState {
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-
-    log4rs::init_file(&args.log_config, Deserializers::new()).expect("Failed to init logger");
+    log4rs::init_file(&args.log_config, Deserializers::new()).unwrap();
     dotenv::from_path(&args.dotenv).ok();
-
     get_external_ip().await.unwrap();
 
-    // ---------- Shared State ----------
+    // ---- Core shared state ----
     let blockchain = Arc::new(Mutex::new(Blockchain::init()));
     let pool = Arc::new(Mutex::new(ConnectionPool::init()));
     let ws_peers = Arc::new(Mutex::new(Vec::new()));
 
     let server = Arc::new(Mutex::new(HandlerServer {
-        blockchain: blockchain.clone(),
+        blockchain,
         connection_pool: pool.clone(),
         p2p_protocol: None,
     }));
@@ -57,50 +55,60 @@ async fn main() {
     server.lock().await.p2p_protocol = Some(p2p.clone());
 
     let app_state = AppState {
-        blockchain,
         pool: pool.clone(),
         p2p,
         ws_peers: ws_peers.clone(),
     };
 
-    // ---------- HTTP + WS ----------
+    // ---- CORS ----
+    let cors = CorsLayer::new()
+        .allow_origin(Any) // during dev
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    // ---- Axum Router ----
     let app = Router::new()
         .route("/transaction", post(submit_transaction))
         .route("/ws", get(ws_handler))
+        .layer(cors)
         .with_state(app_state);
 
-    let http_listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // ---- HTTP + WS Server ----
+    let http_addr: SocketAddr = "0.0.0.0:3000".parse().unwrap();
     tokio::spawn(async move {
-        axum::serve(http_listener, app).await.unwrap();
+        axum::serve(TcpListener::bind(http_addr).await.unwrap(), app)
+            .await
+            .unwrap();
     });
 
-    // ---------- TCP P2P ----------
-    let listener = TcpListener::bind("0.0.0.0:2373").await.unwrap();
-    log::info!("TCP P2P listening on 0.0.0.0:2373");
+    // ---- TCP P2P Server ----
+    let tcp_listener = TcpListener::bind("0.0.0.0:2373").await.unwrap();
+    log::info!("P2P TCP listening on 2373");
 
     loop {
-        let (stream, addr) = listener.accept().await.unwrap();
-        log::info!("TCP peer connected: {}", addr);
+        let (stream, addr) = tcp_listener.accept().await.unwrap();
+        log::info!("P2P connection from {}", addr);
 
         let pool_clone = pool.clone();
         let server_clone = server.clone();
 
         tokio::spawn(async move {
             if let Err(e) = handle_connection(pool_clone, stream, server_clone).await {
-                log::error!("TCP connection error: {:?}", e);
+                log::error!("TCP error: {:?}", e);
             }
         });
     }
 }
 
-// ---------- HTTP TRANSACTION ----------
+// ---------------- HTTP ----------------
+
 async fn submit_transaction(
     State(state): State<AppState>,
     Json(tx_msg): Json<TransactionMessage>,
 ) -> impl IntoResponse {
     let msg_str = serde_json::to_string(&tx_msg).unwrap();
 
-    // ---- P2P Logic ----
+    // ---- Core consensus logic ----
     state
         .p2p
         .lock()
@@ -108,25 +116,25 @@ async fn submit_transaction(
         .handle_transaction(&msg_str, None, Some(state.ws_peers.clone()))
         .await;
 
-    // ---- WebSocket Broadcast ----
+    // ---- WebSocket broadcast ----
     let peers = state.ws_peers.lock().await;
     for peer in peers.iter() {
         let _ = peer.send(Message::Text(msg_str.clone().into()));
     }
 
-    // ---- TCP Broadcast ----
+    // ---- TCP broadcast ----
     let pool = state.pool.lock().await;
     let clients = pool.clients.lock().await;
-
     for client in clients.iter() {
-        let mut w = client.writer.lock().await;
-        let _ = w.write_all(msg_str.as_bytes()).await;
+        let mut writer = client.writer.lock().await;
+        let _ = writer.write_all(msg_str.as_bytes()).await;
     }
 
-    (axum::http::StatusCode::OK, "Transaction received")
+    (axum::http::StatusCode::OK, "Transaction accepted")
 }
 
-// ---------- WS HANDLER ----------
+// ---------------- WebSocket ----------------
+
 async fn ws_handler(ws: WebSocketUpgrade, State(state): State<AppState>) -> impl IntoResponse {
     ws.on_upgrade(move |socket| handle_ws(socket, state))
 }
@@ -137,15 +145,13 @@ async fn handle_ws(socket: WebSocket, state: AppState) {
 
     state.ws_peers.lock().await.push(tx);
 
-    // Send loop
     let send_task = tokio::spawn(async move {
         while let Some(msg) = rx.recv().await {
             let _ = sender.send(msg).await;
         }
     });
 
-    // Receive loop (optional, currently ignored)
-    while let Some(Ok(_msg)) = receiver.next().await {}
+    while let Some(Ok(_)) = receiver.next().await {}
 
     send_task.abort();
 }
